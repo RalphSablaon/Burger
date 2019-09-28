f715c8a4de6b44e223ca3d8a1b581d771a47c918
Ralph Sablaon <rsablaon88@gmail.com>
2019-09-28 12:00:27 -0700

    added JAWSDB stuff to js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,32 @@
 var mysql = require ('mysql');
+var connecton;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Z5hjd875*!",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Z5hjd875*!",
+        database: "burgers_db"
+    });
+};
 
-connection.connect(function(err) {
-    if (err) {
-        console.log("Error connecting: " + err.stack);
-        return;
-    }
-    console.log("Connected as ID: " + connection.threadId);
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Z5hjd875*!",
+//     database: "burgers_db"
+// });
+
+// connection.connect(function(err) {
+//     if (err) {
+//         console.log("Error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("Connected as ID: " + connection.threadId);
+// });
 
 module.exports = connection;
