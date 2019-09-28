eb137368d3f18d209238fed3a04f2c332d9bc29f
Ralph Sablaon <rsablaon88@gmail.com>
2019-09-27 21:49:57 -0700

    initial commit - already started, forgot to push earlier

---
+++ config/orm.js
@@ -0,0 +1,32 @@
+var connection = require ("./connection");
+
+var orm = {
+    
+    selectAll: function(cb) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function(err, res) {
+            if (err) throw err;
+            cb (res);
+        });
+    },
+
+    insertOne: function(burger, cb) {
+        var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
+        connection.query(queryString, function(err, res) {
+            if (err) throw err;
+            cb (res);
+        });
+    },
+
+    updateOne: function(id, cb) {
+        var queryString = "UPDATE burgers SET devoured = true WHERE id = ?";
+        connection.query(queryString, function (err, res) {
+
+      
+        if (err) throw err;
+        cb (res);
+        });
+    }
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double Animal Style', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
