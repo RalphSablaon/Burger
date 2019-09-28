61fe07f222938f188a8f8be8c87553c444ed673d
Ralph Sablaon <rsablaon88@gmail.com>
2019-09-28 08:34:22 -0700

    pretty much finished just need to work on css a bit more

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,15 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Z5hjd875*!",
     database: "burgers_db"
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
+    if (err) {
+        console.log("Error connecting: " + err.stack);
+        return;
+    }
     console.log("Connected as ID: " + connection.threadId);
 });
 
--- config/orm.js
+++ config/orm.js
@@ -12,7 +12,7 @@ var orm = {
 
     insertOne: function(burger, cb) {
         var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
-        connection.query(queryString, function(err, res) {
+        connection.query(queryString, [burger], function(err, res) {
             if (err) throw err;
             cb (res);
         });
@@ -20,9 +20,7 @@ var orm = {
 
     updateOne: function(id, cb) {
         var queryString = "UPDATE burgers SET devoured = true WHERE id = ?";
-        connection.query(queryString, function (err, res) {
-
-      
+        connection.query(queryString, [id], function (err, res) {
         if (err) throw err;
         cb (res);
         });
